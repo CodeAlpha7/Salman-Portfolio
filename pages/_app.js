5b0fc8377e4cd8438dbaa7847b32b95967fcb994
SalmanSyed <azeezsalman1@gmail.com>
2025-03-25 17:00:28 -0400

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,42 @@
-//_app.js
+// pages/_app.js
 import '../styles/globals.css'
-import Layout from '../components/Layout'
 import React, { useState, useEffect } from 'react';
+import Layout from '../components/Layout'
+import EnhancedPageTransition from '../components/EnhancedPageTransition';
+import VignetteOverlay from '../components/VignetteOverlay';
 
 function MyApp({ Component, pageProps }) {
   const [showNavbar, setShowNavbar] = useState(true);
+  const [mounted, setMounted] = useState(false);
+  
+  // Only run client-side
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Pass the setShowNavbar function to components that need to control navbar visibility
+  const updatedPageProps = {
+    ...pageProps,
+    setShowNavbar: setShowNavbar
+  };
+  
+  if (!mounted) {
+    // Return a minimal layout for initial server render
+    return null;
+  }
 
   return (
-    <Layout showNavbar={showNavbar}>
-      <Component {...pageProps} setShowNavbar={setShowNavbar} />
-    </Layout>
-  )
+    <>
+      <EnhancedPageTransition>
+        <Layout showNavbar={showNavbar}>
+          {/* Subtle vignette effect for the entire site - reduced intensity for regular pages */}
+          <VignetteOverlay intensity={0.2} pulsate={true} isIntro={false} />
+          
+          <Component {...updatedPageProps} />
+        </Layout>
+      </EnhancedPageTransition>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
